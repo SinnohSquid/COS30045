5ce20296b3221d8b06912398fe26f7b08701f44c
Sinsui <torterra924@outlook.com>
2025-06-01 09:46:03 +1000

    final little update before the end

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 function init() {
-    // initSbarChart(); // REMOVED: This function is undefined as sbarChart_example.js is not provided.
     initLineChart();
     initAvoidableDeathsChart();
     initCauseOfDeathChart();
